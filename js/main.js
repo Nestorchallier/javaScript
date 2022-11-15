0eecbf489b2f1b6558b89b20fa75bd09547bd599
nestorchallier <nestor.challier@gmail.com>
2022-11-14 21:22:29 -0300

    final del E1 javaScript

--- js/main.js
+++ js/main.js
@@ -3,46 +3,46 @@
 const Pizzas = [
     {
         id: 1,
-        nombre: "Piza de Muzzaerella",
+        nombre: "Pizza de Muzzarella",
         precio: 500,
-        ingredientes:["Maza, salsa,muzzarella, aceitunas"],
+        ingredientes:["Maza: (harina, huevos, aceite), salsa, muzzarella, aceitunas"],
 
     },
     {
         id: 2,
         nombre: "Pizza Napolitana",
         precio: 850,
-        ingredientes: ["Maza, salsa, muzzarella, tomate, ajo, aceitunas"],
-
-    },    {
-        id: 2,
-        nombre: "Pizza Calabresa",
-        precio: 450,
-        ingredientes: ["Maza, salsa, longaniza,  aceitunas"],
+        ingredientes: ["Maza: (harina, huevos, aceite), salsa, muzzarella, tomate, ajo, aceitunas"],
 
     },    {
         id: 3,
-        nombre: "Pizza Fugazeta",
-        precio: 700,
-        ingredientes: ["Maza, muzzarella, cebolla, aceitunas "],
+        nombre: "Pizza Calabresa",
+        precio: 450,
+        ingredientes: ["Maza: (harina, huevos, aceite), salsa, longaniza,  aceitunas"],
 
     },    {
         id: 4,
-        nombre: ["Pizza Fiesta"],
-        precio: 600,
-        ingredientes: ["Maza, salsa, muzzarella, anana, aceitunas"],
+        nombre: "Pizza Fugazeta",
+        precio: 700,
+        ingredientes: ["Maza: (harina, huevos, aceite), muzzarella, cebolla, aceitunas "],
 
     },    {
         id: 5,
-        nombre: "Pizza party",
-        precio: 900,
-        ingredientes: ["Maza, salsa, muzzarella, ajo y perejil, chorrizo colorado, aceitunas"],
+        nombre: ["Pizza Fiesta"],
+        precio: 600,
+        ingredientes: ["Maza: (harina, huevos, aceite), salsa, muzzarella, anana, aceitunas"],
 
     },    {
         id: 6,
+        nombre: "Pizza party",
+        precio: 900,
+        ingredientes: ["Maza: (harina, huevos, aceite), salsa, muzzarella, ajo y perejil, chorrizo colorado, aceitunas"],
+
+    },    {
+        id: 7,
         nombre: "Pizza zapi",
         precio: 680,
-        ingredientes: ["Maza, salsa, muzzarella, roquefort, provolone, aceitunas"],
+        ingredientes: ["Maza: (harina, huevos, aceite), salsa, muzzarella, roquefort, provolone, aceitunas"],
 
     },
 ];
@@ -58,11 +58,38 @@ idImpares.forEach((Pizza) => {
 
 // Precio menor A Ejercicio B
 
-const precioMenorA = (precio) => {
-    const pizzasFiltradas = Pizzas.filter((Pizza) => {
-        return Pizza.precio < precio;
-    });
-    return pizzasFiltradas;
+const hayPizzasConPrecioMenorA = (precio) => {
+    return Pizzas.some((Pizza) => {
+        return Pizza.precio > precio;
+    })
+        ? console.log(`Hay pizzas con precio menor a $${precio}`)
+        : console.log(`No hay pizzas con precio mayor a $${precio}`);
 };
 
-console.log(precioMenorA(600));
+hayPizzasConPrecioMenorA(600)
+
+// El nombre de cada pizza con su respectivo precio Ejercicio C
+
+const NombresDeLasPizzas = Pizzas.filter((Pizzas) => {
+    return Pizzas.nombre && Pizzas.precio;
+}
+);
+
+NombresDeLasPizzas.forEach((Pizzas) => {
+    console.log(
+        `las pizzas que tenemos son, ${Pizzas.nombre} y vale $${Pizzas.precio}.`
+    );
+});
+
+// ingredientes de cada pizza Ejercicio D
+
+const ingredientesDeLasPizzas = Pizzas.filter((Pizzas) => {
+    return Pizzas.nombre && Pizzas.ingredientes;
+}
+);
+
+ingredientesDeLasPizzas.forEach((Pizzas) => {
+    console.log(
+        `Tenemos: ${Pizzas.nombre} los ingredientes son ${Pizzas.ingredientes}.`
+    );
+});
